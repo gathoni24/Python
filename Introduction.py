9fa276d146072473c1c7df69a1d3e302aa5e925b
gathoni24 <traceygathoni007@gmail.com>
2024-02-05 13:15:03 +0300

    commit

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+# Print is a method for displaying output
+print("eMobilis")
+print("Welcome to Python Programming")
+print(100)
+print(5+10)
+print(5*10)
---
+++ variables.py
@@ -0,0 +1,17 @@
+# variable is a container for storing values
+school = "eMobilis"
+firstname = "Tracey"
+lastname = "Wanjiru"
+x = 5
+y = 10
+num1 = 20
+num2 = 3
+# print is a method for displaying output
+print(school)
+print(firstname)
+print(lastname)
+print(x+y)
+print(num1*num2)
+print("I study at",school)
+print("My full name is",firstname,lastname)
+print("The product of",num1,"and",num2,"is",num1*num2)
